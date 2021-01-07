15312b1e364c77c2ee958277fcd08535cf5f5e06
mariamalaidi <mariam.alaide@gmail.com>
2021-01-07 17:46:36 +0200

    Writing red method

---
+++ src/Colors.java
@@ -0,0 +1,9 @@
+public class Colors {
+
+    public void red(){
+        System.out.println("this is red ~");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello !");
+    }
+}
