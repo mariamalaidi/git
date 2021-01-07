3e188bdeb4bc74dd0b83dec424ad4ebeaa9a1d18
mariamalaidi <mariam.alaide@gmail.com>
2021-01-07 18:00:29 +0200

    Basic class

--- src/Colors.java
+++ src/Colors.java
@@ -1,9 +1,8 @@
 public class Colors {
 
-    public void red(){
-        System.out.println("this is red ~");
-    }
+
     public static void main(String[] args) {
         System.out.println("Hello !");
     }
+
 }
